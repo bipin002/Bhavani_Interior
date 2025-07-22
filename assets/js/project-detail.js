40f8150ecc5df7ce713dbac5e80f910defec276f
bipin002 <mistrybipin002@gmail.com>
2025-07-22 11:13:46 -0700

    Change 01-Added client Images 02-align with our design 03-project and projectdetail  added

--- assets/js/project-detail.js
+++ assets/js/project-detail.js
@@ -3,7 +3,7 @@ const projectData = [
   {
     id: "living-room",
     title: "Modern Living Room",
-    mainImage: "assets/images/modern-kitchen-interior-white-colors.jpg",
+    mainImage: "assets/images/Living_room.jpeg",
     description: "Minimalist with cozy tones",
     longDescription: "This modern living room design reflects a harmonious blend of functionality and elegance. Using neutral tones and sleek furniture, the space was crafted to evoke both warmth and sophistication. The open layout allows natural light to amplify the design, while carefully selected decor elements add personality.",
     client: "Mr. Sharma",
@@ -11,14 +11,18 @@ const projectData = [
     size: "400 sq.ft",
     duration: "2 months",
     images: [
-      "assets/images/modern-kitchen-interior-white-colors.jpg",
-      "assets/images/modern-kitchen-interior-white-colors.jpg"
+      "assets/images/Livingroom_02.jpeg",
+      "assets/images/Livingroom_03.jpeg",
+      "assets/images/Livingroom_06.jpeg",
+      "assets/images/Livingroom_08.jpeg",
+      "assets/images/Livingroom_07.jpeg",
+      "assets/images/Livingroom_10.jpeg"
     ]
   },
   {
     id: "kitchen",
     title: "Modern Kitchen",
-    mainImage: "assets/images/minimalist-kitchen-interior-design.jpg",
+    mainImage: "assets/images/Kitchen_01.jpeg",
     description: "Minimalist with cozy tones",
     longDescription: "This modern living room design reflects a harmonious blend of functionality and elegance. Using neutral tones and sleek furniture, the space was crafted to evoke both warmth and sophistication. The open layout allows natural light to amplify the design, while carefully selected decor elements add personality.",
     client: "Mr. Sharma",
@@ -26,14 +30,14 @@ const projectData = [
     size: "400 sq.ft",
     duration: "2 months",
     images: [
-      "assets/images/minimalist-kitchen-interior-design.jpg",
+      "assets/images/Kitchen_01.jpeg",
       "assets/images/minimalist-kitchen-interior-design.jpg"
     ]
   },
   {
-    id: "Wardrobe",
-    title: "Modern Wardrobe",
-    mainImage: "assets/images/elegant-minimal-interior-design.jpg",
+    id: "bedroom",
+    title: "Modern bedroom",
+    mainImage: "assets/images/Bedroom_01.jpg",
     description: "Minimalist with cozy tones",
     longDescription: "This modern living room design reflects a harmonious blend of functionality and elegance. Using neutral tones and sleek furniture, the space was crafted to evoke both warmth and sophistication. The open layout allows natural light to amplify the design, while carefully selected decor elements add personality.",
     client: "Mr. Sharma",
@@ -41,8 +45,16 @@ const projectData = [
     size: "400 sq.ft",
     duration: "2 months",
     images: [
-      "assets/images/elegant-minimal-interior-design.jpg",
-      "assets/images/elegant-minimal-interior-design.jpg"
+      "assets/images/Bedroom_01.jpeg",
+       "assets/images/Bedroom_02.jpeg",
+        "assets/images/Bedroom_03.jpeg",
+         "assets/images/Bedroom_04.jpeg",
+          "assets/images/Bedroom_05.jpeg",
+           "assets/images/Bedroom_06.jpeg",
+            "assets/images/Bedroom_07.jpeg",
+             "assets/images/Bedroom_08.jpeg",
+              "assets/images/Bedroom_09.jpeg",
+               "assets/images/Bedroom_10.jpeg",
     ]
   },
 
