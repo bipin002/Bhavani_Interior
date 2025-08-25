de029b06ae6b01c423975ab685682ebfd7830c51
Bipin Mistry <mistrybipin002@gmail.com>
2025-08-25 23:52:28 +0530

    Change- 01-align project wise images and details

--- assets/js/project-detail.js
+++ assets/js/project-detail.js
@@ -1,6 +1,6 @@
 const projectData = [
   {
-    clientId: "Lodha",
+    clientId: "Rustomjee",
     
     
     projects: [
@@ -10,7 +10,7 @@ const projectData = [
         mainImage: "assets/images/Living_room.jpeg",
         description: "Minimalist with cozy tones",
         longDescription: "A harmonious blend of style and comfort, the living room is designed to be a welcoming space for both relaxation and social gatherings. Featuring modern aesthetics, subtle textures, and well-placed lighting, this room creates an inviting ambiance that feels both spacious and intimate.",
-        clientName: "Lodha",
+        clientName: "Rustomjee Crown",
         location: "Prabhadevi West ,Mumbai",
         
         images: [
@@ -28,13 +28,12 @@ const projectData = [
         mainImage: "assets/images/Kitchen_01.jpeg",
         description: "Minimalist with cozy tones",
         longDescription: "Designed with functionality and elegance in mind, the kitchen combines sleek finishes, ergonomic layout, and efficient storage solutions. Smart appliances and premium materials make this space perfect for everyday cooking and entertaining, all while maintaining a clean and contemporary look.",
-        clientName: "Lodha",
+        clientName:"Rustomjee Crown",
         location: "Prabhadevi West ,Mumbai",
         
         images: [
           "assets/images/Kitchen_01.jpeg",
-          "assets/images/minimalist-kitchen-interior-design.jpg",
-          "assets/images/modern-kitchen-interior-white-colors.jpg"
+         
         ]
       },
       {
@@ -43,7 +42,7 @@ const projectData = [
         mainImage: "assets/images/Bedroom_01.jpg",
         description: "Minimalist with cozy tones",
         longDescription: "The bedroom serves as a personal retreat, offering warmth, privacy, and serenity. With soft tones, ambient lighting, and custom storage, the design emphasizes relaxation and personal comfort, making it the ideal space to unwind after a long day.",
-        clientName: "Lodha",
+        clientName: "Rustomjee Crown",
         location: "Prabhadevi West ,Mumbai",
         
         images: [
@@ -51,26 +50,32 @@ const projectData = [
            "assets/images/Bedroom_02.jpeg",
             "assets/images/Bedroom_03.jpeg",
              "assets/images/Bedroom_04.jpeg",
-              "assets/images/Bedroom_05.jpeg",           
-                "assets/images/Bedroom_07.jpeg",
+              "assets/images/Bedroom_05.jpeg",         
                  "assets/images/Bedroom_08.jpeg",
                   "assets/images/Bedroom_09.jpeg",
-                   "assets/images/Bedroom_10.jpeg",
+                   "assets/images/Bedroom_11.jpeg",
         ]
       },
     
        {
         id: "wardrobe",
         title: "Modern Wardrobe",
-        mainImage: "assets/images/Wardrobe.jpeg",
+        mainImage: "assets/images/Bedroom_07.jpeg",
         description: "Minimalist with cozy tones",
         longDescription: "A modern wardrobe solution that balances aesthetics with maximum utility. Designed with ample storage, customizable compartments, and a clean exterior finish, the wardrobe blends seamlessly into the room while enhancing the overall decor and functionality.",
-        clientName: "Lodha",
+        clientName: "Rustomjee Crown",
         location: "Prabhadevi West ,Mumbai",
         
         images: [
-         "assets/images/Wardrobe.jpeg",
-         "assets/images/Wardrobe_01.jpeg"
+         "assets/images/Bedroom_07.jpeg",
+         "assets/images/Bedroom_10.jpeg",
+         "assets/images/Bedroom_12.jpeg",
+          "assets/images/Wardrobe.jpeg",
+         "assets/images/Wardrobe_01.jpeg",
+          "assets/images/Wardrobe_10.jpg",
+            "assets/images/Bedroom_Wardrobe.jpeg",
+               "assets/images/Bedroom_decoration_04.jpeg",
+        
         ]
       },
       {
@@ -79,7 +84,7 @@ const projectData = [
         mainImage: "assets/images/Dinningroom_01.jpeg",
         description: "Minimalist with cozy tones",
         longDescription: "A stylish and well-lit dining area created to bring families together. The design focuses on comfort and elegance, incorporating statement lighting, ergonomic seating, and a cohesive color palette to elevate the dining experience in both daily meals and formal gatherings.",
-        clientName: "Lodha",
+        clientName: "Rustomjee Crown",
         location: "Prabhadevi West ,Mumbai",
         
         images: [
@@ -95,7 +100,7 @@ const projectData = [
         mainImage: "assets/images/GalleryArea.jpeg",
         description: "Minimalist with cozy tones",
         longDescription: "The gallery section offers a visual journey through our finest work—capturing the elegance, detail, and craftsmanship that define each project. Carefully curated images showcase the transformation of spaces from concept to completion, highlighting our design aesthetics, creative solutions, and attention to detail. This area not only reflects our style but also serves as inspiration for future projects, presenting a range of themes, moods, and spatial innovations.",
-        clientName: "Lodha",
+        clientName:"Rustomjee Crown",
         location: "Prabhadevi West ,Mumbai",
         images: [
           "assets/images/GalleryArea.jpeg",
@@ -104,6 +109,7 @@ const projectData = [
            "assets/images/HomeDecoration.jpeg",
           "assets/images/HomeDecoration_02.jpeg",
           "assets/images/HomeDecoration_03.jpeg",
+          "assets/images/MainGate.jpeg",
         ]
       },
     
@@ -113,7 +119,7 @@ const projectData = [
         mainImage: "assets/images/Toilet.jpeg",
         description: "Minimalist with cozy tones",
         longDescription: "The toilet space is designed for both hygiene and style. Clean lines, quality fixtures, and thoughtful lighting create a fresh and modern look. The layout ensures ease of maintenance while offering a relaxing atmosphere through neutral tones and smart design elements.",
-        clientName: "Lodha",
+        clientName: "Rustomjee Crown",
         location: "Prabhadevi West ,Mumbai",
         images: [
           "assets/images/Toilet.jpeg",
@@ -125,125 +131,125 @@ const projectData = [
   },
 
   {
-    clientId: "Rustomjee",
+    clientId: "Lodha",
     
     
     projects: [
       {
         id: "living-room",
         title: "Modern Living Room",
-        mainImage: "assets/images/Living_room.jpeg",
+        mainImage: "assets/images/Lodha_LivingRoom_03.jpg",
         description: "Minimalist with cozy tones",
         longDescription: "A harmonious blend of style and comfort, the living room is designed to be a welcoming space for both relaxation and social gatherings. Featuring modern aesthetics, subtle textures, and well-placed lighting, this room creates an inviting ambiance that feels both spacious and intimate.",
-        clientName: "Rustomjee",
-        location: "Prabhadevi West ,Mumbai",
-        
+        clientName: "Lodha Park",
+        location: "Prabhadevi East ,Mumbai",
         images: [
-          "assets/images/Livingroom_02.jpeg",
-          "assets/images/Livingroom_03.jpeg",
-          "assets/images/Livingroom_06.jpeg",
-          "assets/images/Livingroom_08.jpeg",
-          "assets/images/Livingroom_07.jpeg",
-          "assets/images/Livingroom_10.jpeg"
+          "assets/images/Lodha_LivingRoom_03.jpg",
+          "assets/images/Lodha_LivingRoom_02.jpg",
+          "assets/images/Lodha_LivingRoom.jpg",
+           "assets/images/Lodha_MainGate.jpg",
+         
         ]
       },
-      {
-        id: "kitchen",
-        title: "Modern Kitchen",
-        mainImage: "assets/images/Kitchen_01.jpeg",
-        description: "Minimalist with cozy tones",
-        longDescription: "Designed with functionality and elegance in mind, the kitchen combines sleek finishes, ergonomic layout, and efficient storage solutions. Smart appliances and premium materials make this space perfect for everyday cooking and entertaining, all while maintaining a clean and contemporary look.",
-        clientName: "Rustomjee",
-        location: "Prabhadevi West ,Mumbai",
-        
-        images: [
-          "assets/images/Kitchen_01.jpeg",
-          "assets/images/minimalist-kitchen-interior-design.jpg",
-          "assets/images/modern-kitchen-interior-white-colors.jpg"
-        ]
-      },
+      
       {
         id: "bedroom",
         title: "Modern bedroom",
-        mainImage: "assets/images/Bedroom_01.jpg",
+        mainImage: "assets/images/Lodha_Bedroom.jpg",
         description: "Minimalist with cozy tones",
         longDescription: "The bedroom serves as a personal retreat, offering warmth, privacy, and serenity. With soft tones, ambient lighting, and custom storage, the design emphasizes relaxation and personal comfort, making it the ideal space to unwind after a long day.",
-        clientName: "Rustomjee",
-        location: "Prabhadevi West ,Mumbai",
+      clientName: "Lodha Park",
+        location: "Prabhadevi East ,Mumbai",
         
         images: [
-          "assets/images/Bedroom_01.jpeg",
-           "assets/images/Bedroom_02.jpeg",
-            "assets/images/Bedroom_03.jpeg",
-             "assets/images/Bedroom_04.jpeg",
-              "assets/images/Bedroom_05.jpeg",           
-                "assets/images/Bedroom_07.jpeg",
-                 "assets/images/Bedroom_08.jpeg",
-                  "assets/images/Bedroom_09.jpeg",
-                   "assets/images/Bedroom_10.jpeg",
+          "assets/images/Lodha_Bedroom.jpg",         
+            
         ]
       },
     
        {
         id: "wardrobe",
         title: "Modern Wardrobe",
-        mainImage: "assets/images/Wardrobe.jpeg",
+        mainImage: "assets/images/Lodha_Wardrobe_01.jpg",
         description: "Minimalist with cozy tones",
         longDescription: "A modern wardrobe solution that balances aesthetics with maximum utility. Designed with ample storage, customizable compartments, and a clean exterior finish, the wardrobe blends seamlessly into the room while enhancing the overall decor and functionality.",
-        clientName: "Rustomjee",
-        location: "Prabhadevi West ,Mumbai",
+       clientName: "Lodha Park",
+        location: "Prabhadevi East ,Mumbai",
         
         images: [
-         "assets/images/Wardrobe.jpeg",
-         "assets/images/Wardrobe_01.jpeg"
+         "assets/images/Lodha_Wardrobe_01.jpg",
+        "assets/images/Lodha_Wardrobe_02.jpg",
+        "assets/images/Lodha_Wardrobe_03.jpg",
+        "assets/images/Lodha_Wardrobe_04.jpg",
         ]
       },
-      {
-        id: "dinningroom",
-        title: "Modern Dinningroom",
-        mainImage: "assets/images/Dinningroom_01.jpeg",
-        description: "Minimalist with cozy tones",
-        longDescription: "A stylish and well-lit dining area created to bring families together. The design focuses on comfort and elegance, incorporating statement lighting, ergonomic seating, and a cohesive color palette to elevate the dining experience in both daily meals and formal gatherings.",
-        clientName: "Rustomjee",
-        location: "Prabhadevi West ,Mumbai",
-        
-        images: [
-          "assets/images/Dinningroom_01.jpeg",
-           "assets/images/Dinningroom_02.jpeg",
-            "assets/images/Dinningroom_03.jpeg",
-        ]
-      },
+      
     
        {
         id: "galleryarea",
         title: "Modern GalleryArea",
-        mainImage: "assets/images/GalleryArea.jpeg",
+        mainImage: "assets/images/Lodha_Gallery.jpg",
         description: "Minimalist with cozy tones",
         longDescription: "The gallery section offers a visual journey through our finest work—capturing the elegance, detail, and craftsmanship that define each project. Carefully curated images showcase the transformation of spaces from concept to completion, highlighting our design aesthetics, creative solutions, and attention to detail. This area not only reflects our style but also serves as inspiration for future projects, presenting a range of themes, moods, and spatial innovations.",
-        clientName: "Rustomjee",
-        location: "Prabhadevi West ,Mumbai",
+       clientName: "Lodha Park",
+        location: "Prabhadevi East ,Mumbai",
         images: [
-          "assets/images/GalleryArea.jpeg",
-          "assets/images/GalleryArea_02.jpeg",
-          "assets/images/GalleryArea_03.jpeg",
-           "assets/images/HomeDecoration.jpeg",
-          "assets/images/HomeDecoration_02.jpeg",
-          "assets/images/HomeDecoration_03.jpeg",
+          "assets/images/Lodha_Gallery.jpg",
+          
         ]
       },
     
        {
-        id: "toilet",
-        title: "Modern Toilet",
-        mainImage: "assets/images/Toilet.jpeg",
-        description: "Minimalist with cozy tones",
-        longDescription: "The toilet space is designed for both hygiene and style. Clean lines, quality fixtures, and thoughtful lighting create a fresh and modern look. The layout ensures ease of maintenance while offering a relaxing atmosphere through neutral tones and smart design elements.",
-        clientName: "Rustomjee",
-        location: "Prabhadevi West ,Mumbai",
+        id: "Pooja Ghar",
+        title: "Modern Pooja Ghar",
+        mainImage: "assets/images/Lodha_PoojaGhar_01.jpg",
+        description: "A serene blend of tradition and modern elegance.",
+        longDescription: "This modern Pooja Ghar is designed to create a calming spiritual space within the home. Subtle lighting, warm wooden tones, and intricate detailing bring a sense of peace and purity. The design balances traditional elements with a minimalist approach, making it both sacred and contemporary. The compact yet graceful layout ensures functionality while fostering a tranquil atmosphere for daily prayers and rituals.",
+       clientName: "Lodha Park",
+        location: "Prabhadevi East ,Mumbai",
         images: [
-          "assets/images/Toilet.jpeg",
-           "assets/images/Toilet_02.jpeg",
-            "assets/images/Toilet_03.jpeg",
+          "assets/images/Lodha_PoojaGhar_01.jpg",
+          "assets/images/Lodha_PoojaGhar.jpg",
+        ]
+      },
+       {
+        id: "Lobby",
+        title: "Modern Lobby",
+        mainImage: "assets/images/Lodha_Lobby.jpg",
+        description: "A welcoming space that exudes elegance and sophistication..",
+        longDescription: "The modern lobby is designed as the perfect first impression of the residence. With sleek finishes, ambient lighting, and a harmonious color palette, it sets a tone of luxury and comfort. The layout emphasizes openness and functionality, while the refined materials and textures highlight contemporary aesthetics. This space creates a warm and inviting atmosphere for residents and guests, seamlessly blending style with comfort.",
+        clientName: "Lodha Park",
+        location: "Prabhadevi East ,Mumbai",
+        images: [
+          "assets/images/Lodha_Lobby.jpg",
+        ]
+      },
+    ]
+  },
+  {
+    clientId: "Godrej",
+    
+    
+    projects: [
+    
+     
+       {
+        id: "lobby",
+        title: "Modern Lobby",
+        mainImage: "assets/images/Godrej_Lobby_01.jpg",
+        description: "A welcoming space that exudes elegance and sophistication..",
+        longDescription: "The modern lobby is designed as the perfect first impression of the residence. With sleek finishes, ambient lighting, and a harmonious color palette, it sets a tone of luxury and comfort. The layout emphasizes openness and functionality, while the refined materials and textures highlight contemporary aesthetics. This space creates a warm and inviting atmosphere for residents and guests, seamlessly blending style with comfort.",
+        clientName: "Godrej Planet Tower 3 & 4",
+        location: "Mahalaxmi Sath Rasta ,Mumbai",
+        images: [
+           "assets/images/Godrej_Lobby_01.jpg",
+            "assets/images/Godrej_Lobby_02.jpg",
+             "assets/images/Godrej_Lobby_03.jpg",
+              "assets/images/Godrej_Lobby_04.jpg",
+               "assets/images/Godrej_Lobby_05.jpg",
+                "assets/images/Godrej_Lobby_06.jpg",
+                 "assets/images/Godrej_Lobby_07.jpg",
+                  "assets/images/Godrej_Lobby_08.jpg",
         ]
       },
     ]
